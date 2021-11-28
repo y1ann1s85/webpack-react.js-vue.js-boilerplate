07b5c746040e5040f60be83bce5905ad5141c3ac
y1ann1s85 <yiannis.alexiou85@gmail.com>
2021-11-28 11:51:10 +0200

    added dynamic import on vue, react

--- resources/src/main.js
+++ resources/src/main.js
@@ -1,3 +1,19 @@
 import '../styles/sass/pages/landing.scss';
-require('./vue/main.js');
-require('./react/main.js');
+
+// Trigger Vue.js
+document.querySelector('#go-vue').addEventListener('click', () => {
+    document.querySelector('.welcome-screen').classList.add('hide');
+    import(
+        /* webpackChunkName: "vue-main" */
+        "./vue/main.js"
+    );
+})
+
+// Trigger React.js
+document.querySelector('#go-react').addEventListener('click', () => {
+    document.querySelector('.welcome-screen').classList.add('hide');
+    import(
+        /* webpackChunkName: "react-main" */
+        "./react/main.js"
+    );
+})
